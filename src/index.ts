1bdd1721b75f7e59156b59c642c5fbe376079c6e
47kyo <68kenichi@gmail.com>
2023-04-09 17:07:32 +0200

    add express support

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const x = "hello worlsd";
-console.log(x);
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
